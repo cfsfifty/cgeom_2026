7e11a3ee23d964784672e3e07442e717460ed2fb
Christoph Fuenfzig <c.fuenfzig@gmx.de>
2026-08-27 14:57:27 +0200

    changed argument in_place to copy_matrix; return transposed matrix in transpose_recursive_inplace

--- example_PyOpenGL/TransposeMemoryAccess_aufgabe1.py
+++ example_PyOpenGL/TransposeMemoryAccess_aufgabe1.py
@@ -6,17 +6,17 @@ import numpy as np
 import time
 import copy
 
-def benchmark(func, matrix, repeats=5, in_place=False):
+def benchmark(func, matrix, repeats=5, copy_matrix=False):
 	times = []
 	for _ in range(repeats):
-		data = copy.deepcopy(matrix) if in_place else matrix
+		data = copy.deepcopy(matrix) if copy_matrix else matrix
 		start = time.perf_counter()
 		func(data)
 		end = time.perf_counter()
 		times.append(end - start)
 	return sum(times) / len(times)
 
-# Python list
+# version using Python list
 def transpose_list_naive(matrix):
 	N = len(matrix)
 	transpose = [[np.float64(0.0) for _ in range(N)] for _ in range(N)]
@@ -24,8 +24,15 @@ def transpose_list_naive(matrix):
 		for j in range(N):
 			transpose[j][i] = matrix[i][j]
 	return transpose
+# version using numpy.array
+def transpose_numpy_naive(matrix):
+	transpose = np.empty_like(matrix)
+	for i in range(transpose.shape[0]):
+		for j in range(transpose.shape[1]):
+			transpose[j][i] = matrix[i][j]
+	return transpose
 
-# cf version using numpy.array; also off-diagonal block is handled recursively
+# version using numpy.array; also off-diagonal block is handled recursively
 def transpose_numpy_recursive(matrix, row=0, col=0, size=None):
 	if size is None:
 		size = matrix.shape[0]
@@ -43,7 +50,7 @@ def transpose_numpy_recursive(matrix, row=0, col=0, size=None):
 					matrix_ij = matrix[row + i, col + j] 
 					matrix[row + i, col + j] = matrix[row + j, col + i] 
 					matrix[row + j, col + i] = matrix_ij
-		return
+		return matrix
 
 	half = size // 2
 
@@ -53,14 +60,7 @@ def transpose_numpy_recursive(matrix, row=0, col=0, size=None):
 	transpose_numpy_recursive(matrix, row, col + half, half)
 	# cf this is not necessary, like 'for j in range(i + 1, size)':
 	#transpose_numpy_recursive(matrix, row + half, col, half)
-
-# version using numpy.array
-def transpose_numpy_naive(matrix):
-	transpose = np.zeros(matrix.shape, dtype=np.float64)
-	for i in range(transpose.shape[0]):
-		for j in range(transpose.shape[1]):
-			transpose[j][i] = matrix[i][j]
-	return transpose
+	return matrix
 
 # version using numpy.array; inplace!
 def transpose_numpy_inplace(matrix):
@@ -72,16 +72,17 @@ def transpose_numpy_inplace(matrix):
 			matrix[j,i] = matrix_ij
 	return matrix
 
-N_values = [100, 500, 1000, 2000, 5000]
+
+N_values = [100, 400, 1000, 2000, 4000]
 for N in N_values:
 	matrix_list = [[np.float64(j) for j in range(N)] for i in range(N)]
-	matrix_np = np.array(matrix_list)
+	matrix_np   = np.array(matrix_list, dtype=np.float64)
 
 	# Benchmarks
 	time_naive   = benchmark(transpose_list_naive, matrix_list)
 	time_naive2  = benchmark(transpose_numpy_naive, matrix_np)
 	time_rowwise = benchmark(transpose_numpy_inplace, matrix_np)
-	time_recursive = benchmark(transpose_numpy_recursive, matrix_np, in_place=True)
+	time_recursive = benchmark(transpose_numpy_recursive, matrix_np)
 	time_numpytranspose = benchmark(np.transpose, matrix_np)
 
 	# Output results
